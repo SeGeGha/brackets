018ef2788fabcfddd973d29b00829dbd3aa7059e
Siarhei Sudakov <s3gaddtv@gmail.com>
2020-03-02 21:05:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var s;
+    let reg = [];
+    for(let item of bracketsConfig) {
+        switch(item.join('')) {
+            case '[]' :
+                reg.push('\\[]');
+                break;
+            case '()':
+                reg.push('\\(\\)');
+                break;
+            case '||' :
+                reg.push('\\|\\|');
+                break;
+            default:
+            reg.push(item.join(''));
+        }
+    }
+    reg = new RegExp(reg.join('|'), 'g')
+    console.log(reg);
+    while ( s != str ) { 
+        s = str;
+        str = str.replace(reg, '')
+    }
+    return !str;
+};
